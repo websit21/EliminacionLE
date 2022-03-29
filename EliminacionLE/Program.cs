a44b507d03aa42191a3a862f5204939e2f10c0ec
didierramirez <drramirez@DESKTOP-PHG449S>
2022-03-29 16:44:06 -0600

    Agregar archivos de proyecto.

---
+++ EliminacionLE/Program.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace listasdoblementeligadas
+{
+    public class listasdoblementeenlazada
+    {
+        Nodo PrimerNodo;
+        Nodo UltimoNodo;
+        Nodo NodoActual;
+        public listasdoblementeenlazada()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+            NodoActual = null;
+        }
+        public bool EstaVacia()
+        {
+            return PrimerNodo == null;
+        }
+        public bool UnicoNodo()
+        {
+            return (PrimerNodo.ligasiguiente == null);
+        }
+        public string Agregar_Nodo_Al_Final()
+        {
+            int Dato;
+            Console.WriteLine("ingrese el elemento que desea insertar");
+            Dato = Convert.ToInt32(Console.ReadLine());
+            Nodo NuevoNodo = new Nodo(Dato);
+            if (EstaVacia())
+            {
+                PrimerNodo = NuevoNodo;
+            }
+            else
+            {
+                UltimoNodo.ligasiguiente = NuevoNodo;
+                NuevoNodo.ligaanterior = UltimoNodo;
+            }
+            UltimoNodo = NuevoNodo;
+            NodoActual = NuevoNodo;
+            return "Dato ingresado satisfactoriamente" + Dato.ToString();
+        }
+                public string Recorrido_Hacia_Adelante()
+        {
+            int totaln = 0;
+            string lista = "";
+            Nodo Nodo_Aux = PrimerNodo;
+            while (Nodo_Aux != null)
+            {
+                lista += Nodo_Aux.Dato + "-->>";
+                Nodo_Aux = Nodo_Aux.ligasiguiente;
+                totaln++;
+            }
+            Console.WriteLine("Los nodos en la lita son: {0}", totaln);
+            lista += "Nulo";
+            return lista;
+        }
+        public string Recorrido_Hacia_Atras()
+        {
+            int TotalNodos = 0;
+            string Lista = "";
+            Nodo Nodo_Aux = UltimoNodo;
+            while (Nodo_Aux != null)
+            {
+                Lista += Nodo_Aux.Dato + "-->>";
+                Nodo_Aux = Nodo_Aux.ligaanterior;
+                TotalNodos++;
+            }
+            Lista += "Nulo";
+            Console.WriteLine("El total de nodos son: {0}", TotalNodos);
+
+            return Lista;
+        }
+
+        public void Forma_Circular()
+        {
+            UltimoNodo.ligasiguiente = PrimerNodo;
+            PrimerNodo.ligaanterior = UltimoNodo;
+        }
+        public void Deshacercircular()
+        {
+
+            UltimoNodo.ligasiguiente = null;
+            PrimerNodo.ligaanterior = null;
+        }
+        public string Eliminar_Nodo_Al_Final()
+        {
+            Nodo NodoSiguiente;
+            Nodo NodoActual;
+            NodoActual = PrimerNodo;
+            NodoSiguiente = NodoActual.ligasiguiente;
+            if (!EstaVacia())
+            {
+                if (UnicoNodo())
+                {
+                    PrimerNodo = null;
+                }
+                else
+                {
+                    while (NodoSiguiente.ligasiguiente != null)
+                    {
+                        NodoActual = NodoSiguiente;
+                        NodoSiguiente = NodoSiguiente.ligasiguiente;
+                    }
+                    NodoSiguiente = null;
+                    NodoActual.ligasiguiente = null;
+                }
+            }
+            else { return "Lista Vacia"; }
+            return "Elemetno eliminado";
+        }
+        public string Eliminar_Al_Inicio_RECORRIENDO_HACIA_ATRAS()
+        {
+            Nodo NodoSiguiente;
+            Nodo NodoActual;
+            NodoActual = UltimoNodo;
+            NodoSiguiente = NodoActual.ligaanterior;
+            if (!EstaVacia())
+            {
+                if (UnicoNodo())
+                {
+                    PrimerNodo = null;
+                }
+                else
+                {
+                    while (NodoSiguiente.ligaanterior != null)
+                    {
+                        NodoActual = NodoSiguiente;
+                        NodoSiguiente = NodoSiguiente.ligaanterior;
+                    }
+                    NodoSiguiente = null;
+                    NodoActual.ligaanterior = null;
+                }
+            }
+            else { return "Esta Vacia"; }
+            return "eliminado";
+        }
+        public string Eliminar_Nodo_Al_Incio()
+        {
+            Nodo NodoAuxiliar;
+            NodoAuxiliar = PrimerNodo;
+            if (!EstaVacia())
+            {
+                if (UnicoNodo())
+                {
+                    PrimerNodo = null;
+                }
+                else
+                {
+                    NodoAuxiliar = PrimerNodo;
+                    PrimerNodo = NodoAuxiliar.ligasiguiente;
+                    PrimerNodo.ligaanterior = null;
+                }
+            }
+            else { return "ESTA VAcia"; }
+            return "eliminado";
+        }
+        public string Eliminar_Nodo_Antes_De_Valor_Especifico()
+        {
+            int ValorNodo;
+            Nodo NodoAuxiliar;
+            Nodo NodoActual;
+            Nodo NodoSiguiente;
+            if (!EstaVacia())
+            {
+                if (UnicoNodo())
+                {
+                    int Respuesta;
+                    Console.WriteLine("hay un solo nodo en la lista lo desea eliminar ? ");
+                    Console.WriteLine(" Si = 1 No = 0");
+                    Respuesta = Convert.ToInt32(Console.ReadLine());
+                    if (Respuesta == 1)
+                    {
+                        PrimerNodo = null;
+
+                    }
+                    if (Respuesta == 2)
+                    {
+                        return "no se elimino";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese el Valor del nodo de referencia que desea eliminar");
+                    ValorNodo = Convert.ToInt32(Console.ReadLine());
+                    if (PrimerNodo.Dato == ValorNodo)
+                    {
+                        Console.WriteLine("El valor que dio es el valor del primer nodo");
+                        return "No se elimino ningun Nodo";
+                    }
+                    else
+                    {
+                        NodoActual = PrimerNodo;
+                        NodoSiguiente = NodoActual.ligasiguiente;
+                        NodoAuxiliar = NodoActual;
+                        if (NodoSiguiente.Dato == ValorNodo)
+                        {
+                            //SE PUEDE UTILIZAR EL METODO DE ELIMINAR AL INICIO*****
+                            NodoAuxiliar = NodoSiguiente;
+                            NodoSiguiente = NodoSiguiente.ligasiguiente;
+                            NodoActual.ligasiguiente = NodoSiguiente;
+                            NodoSiguiente.ligaanterior = NodoActual;
+                            NodoAuxiliar = null;
+                        }
+                        else
+                        {
+                            while (NodoSiguiente.Dato != ValorNodo)
+                            {
+                                NodoAuxiliar = NodoActual;
+                                NodoActual = NodoActual.ligasiguiente;
+                                NodoSiguiente = NodoSiguiente.ligasiguiente;
+                            }
+                            NodoAuxiliar.ligasiguiente = NodoSiguiente;
+                            NodoSiguiente.ligaanterior = NodoAuxiliar;
+                            NodoActual = null;
+                        }
+                        return "Elemento Eliminado";
+                    }
+                }
+
+            }
+            else { return "Esta Vacia"; }
+            return "";
+        }
+        public string Eliminar_Nodo_Despues_De_Valor_Especifico()
+        {
+            int ValorNodo;
+            Nodo NodoAuxiliar;
+            Nodo NodoActual;
+            Nodo NodoSiguiente;
+            if (!EstaVacia())
+            {
+                if (UnicoNodo())
+                {
+                    PrimerNodo = null;
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese el Valor del nodo de referencia que desea eliminar");
+                    ValorNodo = Convert.ToInt32(Console.ReadLine());
+                    NodoActual = PrimerNodo;
+                    NodoSiguiente = NodoActual.ligasiguiente;
+                    NodoAuxiliar = NodoSiguiente.ligasiguiente;
+                    while (NodoActual.Dato != ValorNodo)
+                    {
+                        NodoActual = NodoActual.ligasiguiente;
+                        NodoSiguiente = NodoSiguiente.ligasiguiente;
+                        NodoAuxiliar = NodoSiguiente.ligasiguiente;
+                    }
+                    NodoActual.ligasiguiente = NodoAuxiliar;
+                    NodoAuxiliar.ligaanterior = NodoActual;
+                    NodoSiguiente = null;
+                }
+            }
+            else { return "Esta Vacia"; }
+            return "Elemetno eliminado";
+        }
+        public string Eliminar_Un_Valor_Especifico()
+        {
+            int ValorNodo;
+            Nodo NodoAuxiliar;
+            Nodo NodoActual;
+            Nodo NodoSiguiente;
+            if (!EstaVacia())
+            {
+                if (UnicoNodo())
+
+                {
+                    PrimerNodo = null;
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese el Valor del nodo que desea eliminar");
+                    ValorNodo = Convert.ToInt32(Console.ReadLine());
+                    NodoActual = PrimerNodo;
+                    NodoSiguiente = NodoActual.ligasiguiente;
+                    NodoAuxiliar = NodoActual;
+                    if (PrimerNodo.Dato == ValorNodo)
+                    {
+                        Eliminar_Nodo_Al_Incio();
+
+                        //
+                    }
+                    if (UltimoNodo.Dato == ValorNodo)
+                    {
+                        Eliminar_Nodo_Al_Final();
+                    }
+                    else
+                    {
+                        while (NodoActual.Dato != ValorNodo)
+                        {
+                            NodoAuxiliar = NodoActual;
+                            NodoActual = NodoActual.ligasiguiente;
+                            NodoSiguiente = NodoSiguiente.ligasiguiente;
+                        }
+                        NodoAuxiliar.ligasiguiente = NodoSiguiente;
+                        NodoSiguiente.ligaanterior = NodoAuxiliar;
+                        NodoActual = null;
+                    }
+                    if (UltimoNodo.Dato == ValorNodo)
+
+                    {
+                        Console.WriteLine("El nodo que eliminara es el ultimo nodo");
+                        NodoActual = UltimoNodo;
+                        NodoSiguiente = NodoActual.ligaanterior;
+                        NodoSiguiente.ligasiguiente = null;
+                        NodoActual = null;
+                        UltimoNodo = NodoSiguiente;
+                    }
+                }
+            }
+            else { return "Esta Vacia"; }
+            return "";
+        }
+    }
+
+
+}
+
---
+++ EliminacionLE/menu.cs
@@ -0,0 +1,122 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace listasdoblementeligadas
+{
+    public class Nodo
+    {
+        public Nodo ligaanterior { get; set; }
+        public int Dato { get; set; }
+        public Nodo ligasiguiente { get; set; }
+        public Nodo(int Dato)
+        {
+            ligaanterior = null;
+            this.Dato = Dato;
+            ligasiguiente = null;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            listasdoblementeenlazada lista = new listasdoblementeenlazada();
+            int res;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("--------------LISTAS DOBLEMENTE LIGADAS------------------\n\n");
+                Console.WriteLine("\n\t1. INSERCION DE NODOS");
+                Console.WriteLine("\n\t2. ELIMINACION DE NODOS");
+                Console.WriteLine("\n\t3. RECORRIDO HACIA ADELANTE");
+                Console.WriteLine("\n\t5. SALIR");
+                Console.WriteLine("\nSELECCIONE UNA OPCION");
+                res = Convert.ToInt32(Console.ReadLine());
+                switch (res)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("----------------INSERCION NODOS------------\n\n");
+                        Console.WriteLine("\n\t1. INSERTAR NODO");
+                        Console.WriteLine("\n\t2. REGRESAR AL MENU PRINCIPAL");
+                        Console.WriteLine("\t\t\t SELECCIONE UNA OPCION");
+                        res = Convert.ToInt32(Console.ReadLine());
+                        switch (res)
+                        {
+                            case 1:
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                Console.WriteLine("\t\t --------INSERTAR AL INICIO------ - ");
+                                Console.WriteLine(lista.Agregar_Nodo_Al_Final());
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                break;
+                           
+                           
+                            case 2:
+                        break;
+                        }
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("\t\t-------------ELIMINADO NODOS---------- - ");
+                        Console.WriteLine("\n\t1. ELIMINAR AL INICIO");
+                        Console.WriteLine("\n\t2. ELIMINAR AL FINAL");
+                        Console.WriteLine("\n\t3. ELIMINAR UN ELEMENTO ANTES DE UN DATO X");
+                        Console.WriteLine("\n\t4. ELIMINAR UN ELEMENTO DESPUÉS DE UN DATO X");
+                        Console.WriteLine("\n\t5. ELIMINAR UN NODO EN UNA POSICIÓN ESPECÍFICA");
+                        Console.WriteLine("\n\t6. REGRESAR AL MENU PRINCIPAL");
+                        Console.WriteLine("\t\t\t SELECCIONE UNA OPCION");
+                        res = Convert.ToInt32(Console.ReadLine());
+                        switch (res)
+                        {
+                            case 1:
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                Console.WriteLine("\t\tELIMINAR AL INICIO");
+                                Console.WriteLine(lista.Eliminar_Nodo_Al_Incio());
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                break;
+                            case 2:
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                Console.WriteLine("\t\tELIMINAR AL FINAL");
+                                Console.WriteLine(lista.Eliminar_Nodo_Al_Final());
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                break;
+                            case 3:
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                Console.WriteLine("\t\tELIMINAR UN NODO ANTES DE UN VALOR DADO COMO REFERENCIA");
+                                Console.WriteLine(lista.Eliminar_Nodo_Antes_De_Valor_Especifico());
+                        Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                break;
+                            case 4:
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                Console.WriteLine("\t\tEliminar NODO DESPUES DE UN VALOR DADO COMO REFERENCIA");
+                                Console.WriteLine(lista.Eliminar_Nodo_Despues_De_Valor_Especifico());
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                break;
+                            
+                            case 5:
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                Console.WriteLine("ELIMINAR NODO CON VALOR ESPECIFICO");
+                                Console.WriteLine(lista.Eliminar_Un_Valor_Especifico());
+                                Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                                break;
+                            case 6:
+                                break;
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("\t\t\t--------RECORRIDO HACIA ADELANTE----------");
+                        Console.WriteLine(lista.Recorrido_Hacia_Adelante());
+                        break;
+                   
+                    case 4:
+                        break;
+                          }
+                Console.WriteLine("CONTINUAR EN EL PROGRAMA SI = 1 NO = 0");
+                res = Convert.ToInt32(Console.ReadLine());
+            } while (res == 1);
+        }
+    }
+}
